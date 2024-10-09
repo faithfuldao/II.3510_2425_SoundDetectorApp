f0541102ed492df21526309d3a81fac5176a83bd
faithfuldao <victor.dubrana2@gmail.com>
2024-10-09 11:07:25 +0200

    Record/Stop Audio, Notification when Threshold crossed

--- app/src/main/java/com/example/sounddetector/MainActivity.java
+++ app/src/main/java/com/example/sounddetector/MainActivity.java
@@ -1,24 +1,195 @@
 package com.example.sounddetector;
 
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.app.NotificationCompat;
+
+import android.content.pm.PackageManager;
+import android.media.MediaPlayer;
+import android.media.MediaRecorder;
+import android.os.Environment;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import java.io.IOException;
+
+
+import static android.Manifest.permission.RECORD_AUDIO;
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView startTV, stopTV, playTV, stopplayTV, statusTV;
+
+    private MediaRecorder mRecorder;
+
+    private static String mFileName = null;
+
+    public static final int REQUEST_AUDIO_PERMISSION_CODE = 1;
+
+    //defining threshold for the alert notifications
+    private static final int THRESHOLD_DB = 70;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        statusTV = findViewById(R.id.idTVstatus);
+        startTV = findViewById(R.id.btnRecord);
+        stopTV = findViewById(R.id.btnStop);
+        playTV = findViewById(R.id.btnPlay);
+        stopplayTV = findViewById(R.id.btnStopPlay);
+        stopTV.setBackgroundColor(getResources().getColor(R.color.gray));
+        playTV.setBackgroundColor(getResources().getColor(R.color.gray));
+        stopplayTV.setBackgroundColor(getResources().getColor(R.color.gray));
+
+        startTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startRecording();
+            }
         });
+        stopTV.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pauseRecording();
+
+            }
+        });
+
+        //check frequently the sound measured by the app
+        handler.postDelayed(soundLevelChecker, 1000);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case REQUEST_AUDIO_PERMISSION_CODE:
+                if (grantResults.length > 0) {
+                    boolean permissionToRecord = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+                    boolean permissionToStore = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+                    if (permissionToRecord && permissionToStore) {
+                        Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_LONG).show();
+                    }
+                }
+                break;
+        }
+    }
+
+    public boolean checkPermissions() {
+        int result = ContextCompat.checkSelfPermission(getApplicationContext(), WRITE_EXTERNAL_STORAGE);
+        int result1 = ContextCompat.checkSelfPermission(getApplicationContext(), RECORD_AUDIO);
+        return result == PackageManager.PERMISSION_GRANTED && result1 == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void RequestPermissions() {
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{RECORD_AUDIO, WRITE_EXTERNAL_STORAGE}, REQUEST_AUDIO_PERMISSION_CODE);
+    }
+
+    private void startRecording() {
+        if(checkPermissions()){
+            stopTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+            startTV.setBackgroundColor(getResources().getColor(R.color.gray));
+
+            mFileName = Environment.getExternalStorageDirectory().getAbsolutePath();
+            mFileName += "/AudioRecording.3gp";
+            mRecorder = new MediaRecorder();
+
+
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+
+            mRecorder.setOutputFile(mFileName);
+            try {
+                mRecorder.prepare();
+            } catch (IOException e) {
+                Log.e("TAG", "prepare() failed");
+            }
+
+            mRecorder.start();
+            statusTV.setText("Recording Started");
+        } else {
+            RequestPermissions();
+        }
+        }
+
+    public void pauseRecording() {
+        stopTV.setBackgroundColor(getResources().getColor(R.color.gray));
+        startTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        playTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+        stopplayTV.setBackgroundColor(getResources().getColor(R.color.purple_200));
+
+        mRecorder.stop();
+
+        mRecorder.release();
+        mRecorder = null;
+        statusTV.setText("Recording Stopped");
+    }
+
+    //converting the amplitude measured to decibel for our app
+    public double calculateDecibels(int amplitude) {
+        if (amplitude > 0) {
+            return 20 * Math.log10((double) amplitude / 32767);
+        } else {
+            return 0;
+        }
+    }
+
+
+    private Runnable soundLevelChecker = new Runnable() {
+        @Override
+        public void run() {
+            int amplitude = mRecorder.getMaxAmplitude(); //get sound level
+            double soundDecibelMeasured = calculateDecibels(amplitude); //convert it to decibel
+            if (soundDecibelMeasured > THRESHOLD_DB) {
+                sendNotification();  //send notification if sound crosses 70 db
+            }
+            handler.postDelayed(this, 1000);  // Re-check every second
+        }
+    };
+
+    private void sendNotification() {
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        String channelId  = "Sound_Notification_Channel";
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(channelId, "Sound Notification", NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        Notification notification = new NotificationCompat.Builder(this, channelId)
+                .setSmallIcon(R.drawable.icnotification)
+                .setContentTitle("Sound Alert")
+                .setContentText("Ambiant sound crossed 70db")
+                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .build();
+
+        notificationManager.notify(1, notification);
     }
 }
+
+
+
+
+
+
+
