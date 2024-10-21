492b098f3699a19d327e512db4b346924c57f854
NicolasHing <nicolas.hing@gmail.com>
2024-10-21 12:22:15 +0200

    better layout for graph setting

--- app/src/main/java/com/example/sounddetector/Fragment2.java
+++ app/src/main/java/com/example/sounddetector/Fragment2.java
@@ -1,5 +1,6 @@
 package com.example.sounddetector;
 
+import android.annotation.SuppressLint;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.os.Bundle;
@@ -14,13 +15,19 @@ import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
-import java.util.Objects;
-
 public class Fragment2 extends Fragment {
 
-    private TextView dateTextView;
-    private TextView timeTextView;
-    private Button pickDateAndTimeButton;
+    private TextView startDateTextView;
+    private TextView startTimeTextView;
+    private TextView endDateTextView;
+    private TextView endTimeTextView;
+
+    private Button pickStartDateButton;
+    private Button pickStartTimeButton;
+    private Button pickEndDateButton;
+    private Button pickEndTimeButton;
+
+    private Button plotButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,55 +35,109 @@ public class Fragment2 extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_2, container, false);
 
-        dateTextView = view.findViewById(R.id.date_view);
-        timeTextView = view.findViewById(R.id.time_view);
-        pickDateAndTimeButton = view.findViewById(R.id.pick_date_and_time_button);
+        startDateTextView = view.findViewById(R.id.start_date_view);
+        startTimeTextView = view.findViewById(R.id.start_time_view);
+        pickStartDateButton = view.findViewById(R.id.pick_start_date_button);
+        pickStartTimeButton = view.findViewById(R.id.pick_time_button);
+
+        endDateTextView = view.findViewById(R.id.end_date_view);
+        endTimeTextView = view.findViewById(R.id.end_time_view);
+        pickEndDateButton = view.findViewById(R.id.pick_end_date_button);
+        pickEndTimeButton = view.findViewById(R.id.pick_end_time_button);
+        plotButton = view.findViewById(R.id.plot_button);
 
 
-        pickDateAndTimeButton.setOnClickListener(new View.OnClickListener() {
+        pickStartDateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
-                openDatePicker(); // Open date picker dialog
-
-                openTimePicker(); //Open time picker dialog
+                openStartDatePicker();
             }
         });
+        pickStartTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openStartTimePicker();
+            }
+        });
+        pickEndDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openEndDatePicker();
+            }
+        });
+        pickEndTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openEndTimePicker();
+            }
+        });
+
+        plotButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                plotGraph();
+            }
+        });
+
         return view;
     }
 
 
-    private void openDatePicker() {
+    private void openStartDatePicker() {
         DatePickerDialog datePickerDialog = new DatePickerDialog(requireContext(), R.style.DialogTheme, new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker datePicker, int year, int month, int day) {
-
                 //Showing the picked value in the textView
-                String dateString = String.valueOf(day) + "-" + String.valueOf(month) + "-" + String.valueOf(year);
-                dateTextView.setText(dateString);
-
+                month++;
+                String dateString = String.valueOf(year) + "-" + String.valueOf(month) + "-" + String.valueOf(day);
+                startDateTextView.setText(dateString);
             }
         }, 2024, 10, 20);
-
         datePickerDialog.show();
     }
-
-
-    private void openTimePicker () {
-
+    private void openStartTimePicker () {
         TimePickerDialog timePickerDialog = new TimePickerDialog(requireContext(), R.style.DialogTheme, new TimePickerDialog.OnTimeSetListener() {
             @Override
             public void onTimeSet(TimePicker timePicker, int hour, int minute) {
-
-
                 //Showing the picked value in the textView
                 String timeString = String.valueOf(hour) + ":" + String.valueOf(minute) + ":00";
-                timeTextView.setText(timeString);
-
+                startTimeTextView.setText(timeString);
             }
         }, 15, 30, true);
-
         timePickerDialog.show();
+    }
+    private void openEndDatePicker() {
+        DatePickerDialog datePickerDialog = new DatePickerDialog(requireContext(), R.style.DialogTheme, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                //Showing the picked value in the textView
+                month++;
+                String dateString = String.valueOf(year) + "-" + String.valueOf(month) + "-" + String.valueOf(day);
+                endDateTextView.setText(dateString);
+            }
+        }, 2024, 10, 20);
+        datePickerDialog.show();
+    }
+    private void openEndTimePicker () {
+        TimePickerDialog timePickerDialog = new TimePickerDialog(requireContext(), R.style.DialogTheme, new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+                //Showing the picked value in the textView
+                String timeString = String.valueOf(hour) + ":" + String.valueOf(minute) + ":00";
+                endTimeTextView.setText(timeString);
+            }
+        }, 15, 30, true);
+        timePickerDialog.show();
+    }
+
+    private void plotGraph() {
+        String startDate = startDateTextView.getText().toString();
+        String startTime = startTimeTextView.getText().toString();
+        String endDate = endDateTextView.getText().toString();
+        String endTime = endTimeTextView.getText().toString();
+
+        String start = startDate + " " + startTime;
+        String end = endDate + " " + endTime;
     }
 }
 
